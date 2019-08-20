d5f89ad9dc455c2444f591fe8dc494d99e5f0c11
avi <hi@avi.im>
2019-08-20 08:45:22 +0530

    update inference code

--- commons.py
+++ commons.py
@@ -1,13 +1,18 @@
 import io
 
-
+import torch
+import torch.nn as nn
 from PIL import Image
 from torchvision import models
 import torchvision.transforms as transforms
 
 
 def get_model():
+    checkpoint_path = 'model_state.pt'
     model = models.densenet121(pretrained=True)
+    model.classifier = nn.Linear(1024, 2)
+    model.load_state_dict(torch.load(
+        checkpoint_path, map_location='cpu'), strict=False)
     model.eval()
     return model
 
@@ -21,12 +26,3 @@ def transform_image(image_bytes):
                                             [0.229, 0.224, 0.225])])
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
-
-
-# ImageNet classes are often of the form `can_opener` or `Egyptian_cat`
-# will use this method to properly format it so that we get
-# `Can Opener` or `Egyptian Cat`
-def format_class_name(class_name):
-    class_name = class_name.replace('_', ' ')
-    class_name = class_name.title()
-    return class_name
--- inference.py
+++ inference.py
@@ -1,9 +1,6 @@
-import json
-
 from commons import get_model, transform_image
 
 model = get_model()
-imagenet_class_index = json.load(open('imagenet_class_index.json'))
 
 
 def get_prediction(image_bytes):
@@ -13,5 +10,6 @@ def get_prediction(image_bytes):
     except Exception:
         return 0, 'error'
     _, y_hat = outputs.max(1)
-    predicted_idx = str(y_hat.item())
-    return imagenet_class_index[predicted_idx]
+    if str(y_hat.item()) == '1':
+        return "Malignant"
+    return "Benign"
